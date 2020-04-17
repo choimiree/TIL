f6bdf036103bc294adcb053d87f7d00cca13ab94
miree0612 <miree0612@gmail.com>
2020-04-17 23:25:17 +0900

    0417

--- Problem Solving/SWEA/D2/5174.subtree.py
+++ Problem Solving/SWEA/D2/5174.subtree.py
@@ -1,3 +1,43 @@
 #트리의 일부를 서브트리라고 한다. 주어진 이진 트리에서 노드 N을 루트로 하는 서브 트리에 속한 노드의 개수를 알아내는 프로그램을 만드시오.
 #주어지는 트리는 부모와 자식 노드 번호 사이에 특별한 규칙이 없고, 부모가 없는 노드가 전체의 루트 노드가 된다.
 #이런 경우의 트리는 부모 노드를 인덱스로 다음과 같은 방법으로 나타낼 수 있다. 자식 노드가 0인 경우는 노드가 자식이 없는 경우이다.
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+class Tree:
+    def __init__(self, cnt):
+        self.node_lst = [None]
+        for i in range(E + 1):
+            self.node_lst.append(Node(i))
+
+    def put(self, parent, child):
+        if self.node_lst[parent].left == None:
+            self.node_lst[parent].left = self.node_lst[child]
+        else:
+            self.node_lst[parent].right = self.node_lst[child]
+
+    def count(self, node):
+        self.cnt += 1
+        if node.left != None:
+            self.count(node.left)
+        if node.right != None:
+            self.count(node.right)
+
+    def result(self, num):
+        self.cnt = 0
+        self.count(self.node_lst[num])
+        return self.cnt
+
+
+T = int(input())
+for t in range(1, T+1):
+    E, N = map(int, input().split())
+    put_list = list(map(int, input().split()))
+    tree = Tree(E)
+    for i in range(E):
+        tree.put(put_list[2 * i], put_list[2 * i + 1])
+    print('#{} {}'.format(t, tree.result(N)))
--- Problem Solving/SWEA/D3/5178.노드의합.py
+++ Problem Solving/SWEA/D3/5178.노드의합.py
@@ -1,3 +1,15 @@
 #완전이진트리의 리프 노드에 1000이하의 자연수가 저장되어 있고, 리프 노드를 제외한 노드에는 자식 노드에 저장된 값의 합이 들어있다고 한다.
 #다음은 리프 노드에 저장된 1,2,3이 주어졌을 때, 나머지 노드에 자식 노드의 합을 저장한 예이다.
 #N개의 노드를 갖는 완전 이진 트리의 노드 번호는 루트가 1번이 되며, 같은 단계에서는 왼쪽에서 오른쪽으로 증가, 단계가 꽉 차면 다음단계의 왼쪽부터 시작된다.
+def binary_tree(L, tree):
+    if L > (len(tree)-1): return 0
+    return tree[L] if tree[L] != 0 else binary_tree(L * 2, tree) + binary_tree(L*2 + 1, tree)
+
+for tc in range(int(input())):
+    N,M,L = map(int, input().split())
+    tree = [0] * (N+1)
+    for _ in range(M):
+        x, y = map(int,input().split())
+        tree[x] = y
+    result = binary_tree(L, tree)
+    print('#{} {}'.format(tc+1, result))
